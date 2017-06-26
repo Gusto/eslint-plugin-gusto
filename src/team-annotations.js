ac913a7f8f58054b8c12a8448e43f096ff8028a3
Nikhil Mathew <nikhilmat@gmail.com>
2017-06-26 13:08:06 -0700

    Add team annotations rule

---
+++ src/team-annotations.js
@@ -0,0 +1,91 @@
+function teamAnnotations(context) {
+  const { missions, teams } = (context.options[0] || {});
+
+  const sourceCode = context.getSourceCode();
+  const comments = sourceCode.getAllComments();
+
+  const MISSION_REGEX = / @mission ([\w| ]*)/;
+  function getMissionTag() {
+    return comments.find((comment) => {
+      return comment.loc.start.line === 1 && comment.value.match(MISSION_REGEX);
+    });
+  }
+
+  function isValidMission(missionTag) {
+    return missions.includes(missionTag.value.match(MISSION_REGEX)[1]);
+  }
+
+  const TEAM_REGEX = / @team ([\w| ]*)/;
+  function getTeamTag() {
+    return comments.find((comment) => {
+      return comment.loc.start.line === 2 && comment.value.match(TEAM_REGEX);
+    });
+  }
+
+  function isValidTeam(teamTag) {
+    return teams.includes(teamTag.value.match(TEAM_REGEX)[1]);
+  }
+
+  return {
+    Program() {
+      const missionTag = getMissionTag();
+      if (!missionTag) {
+        const report = {
+          node: { loc: { start: { line: 1, column: 0 } }},
+          message: 'A mission annotation is required at the top of the file (i.e. `# @mission Owning Mission`).'
+        };
+        context.report(report);
+      } else {
+        const isValid = isValidMission(missionTag);
+        if (!isValid) {
+          const report = {
+            node: missionTag,
+            message: 'The mission annotation must be a valid mission.'
+          };
+          context.report(report);
+        }
+      }
+
+      const teamTag = getTeamTag();
+      if (!teamTag) {
+        const report = {
+          node: { loc: { start: { line: 2, column: 0 } }},
+          message: 'A team annotation is required at the second line of the file (i.e. `# @team Owning Team`).'
+        };
+        context.report(report);
+      }  else {
+        const isValid = isValidTeam(teamTag);
+        if (!isValid) {
+          const report = {
+            node: teamTag,
+            message: 'The team annotation must be a valid team.'
+          };
+          context.report(report);
+        }
+      }
+
+      return;
+    }
+  };
+}
+
+teamAnnotations.schema = [{
+  'type': 'object',
+  'properties': {
+    'missions': {
+      'type': 'array',
+      'items': {
+        'type': 'string'
+      }
+    },
+    'teams': {
+      'type': 'array',
+      'items': {
+        'type': 'string'
+      }
+    }
+  },
+  'additionalProperties': false
+}];
+
+export default teamAnnotations;
