ece300f7ac0ffc4060a23d5cabf8f429979a899f
Nikhil Mathew <nikhilmat@gmail.com>
2017-06-27 09:17:04 -0700

    Add team annotations rule (#1)
    * Add team annotations rule
    
    * Node 5 compat

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import globalPayrollProperties from './global-payroll-properties';
 import stringLiteralBlacklist from './string-literal-blacklist';
+import teamAnnotations from './team-annotations';
 
 export const rules = {
   'global-payroll-properties': globalPayrollProperties,
-  'string-literal-blacklist': stringLiteralBlacklist
+  'string-literal-blacklist': stringLiteralBlacklist,
+  'team-annotations': teamAnnotations
 };
---
+++ tests/lib/rules/team-annotations.js
@@ -0,0 +1,107 @@
+import rule from '../../../src/team-annotations';
+import { RuleTester } from 'eslint';
+
+const options = [
+  {
+    missions: [ 'Mission' ],
+    teams: [ 'Team' ],
+  }
+];
+
+const ruleTester = new RuleTester({
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    }
+  }
+});
+ruleTester.run('team-annotations', rule, {
+  valid: [
+    {
+      code: [
+        '// @mission Mission',
+        '// @team Team'
+      ].join("\n"),
+      options,
+      env: {
+        es6: true
+      },
+      ecmaFeatures: {
+        jsx: true
+      }
+    },
+  ],
+  invalid: [
+    {
+      code: [
+        '// @mission Mission'
+      ].join("\n"),
+      options,
+      env: {
+        es6: true
+      },
+      ecmaFeatures: {
+        jsx: true
+      },
+      errors: [{ message: "A team annotation is required at the second line of the file (i.e. `# @team Owning Team`)." }]
+    },
+    {
+      code: [
+        '// @mission Fake Mission',
+        '// @team Team'
+      ].join("\n"),
+      options,
+      env: {
+        es6: true
+      },
+      ecmaFeatures: {
+        jsx: true
+      },
+      errors: [{ message: "The mission annotation must be a valid mission." }]
+    },
+    {
+      code: [
+        '',
+        '// @team Team'
+      ].join("\n"),
+      options,
+      env: {
+        es6: true
+      },
+      ecmaFeatures: {
+        jsx: true
+      },
+      errors: [{ message: "A mission annotation is required at the top of the file (i.e. `# @mission Owning Mission`)." }]
+    },
+    {
+      code: [
+        '// @mission Mission',
+        '// @team Fake Team'
+      ].join("\n"),
+      options,
+      env: {
+        es6: true
+      },
+      ecmaFeatures: {
+        jsx: true
+      },
+      errors: [{ message: "The team annotation must be a valid team." }]
+    },
+    {
+      code: [
+        'function print1() { console.log(1); }',
+      ].join("\n"),
+      options,
+      env: {
+        es6: true
+      },
+      ecmaFeatures: {
+        jsx: true
+      },
+      errors: [
+        { message: "A mission annotation is required at the top of the file (i.e. `# @mission Owning Mission`)." },
+        { message: "A team annotation is required at the second line of the file (i.e. `# @team Owning Team`)." }
+      ]
+    },
+  ]
+});
