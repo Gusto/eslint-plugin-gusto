62473df32205306422f84345eab30b678a6e825d
Mark Wheeler <mark.wheeler@gusto.com>
2019-07-04 09:01:12 -0700

    fix index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import globalPayrollProperties from './global-payroll-properties';
-import react16Instances from './react16-instances';
+import noRestrictedCallResultUse from './no-restricted-call-result-use';
 import stringLiteralBlacklist from './string-literal-blacklist';
 import teamAnnotations from './team-annotations';
 
 // eslint-disable-next-line import/prefer-default-export
 export const rules = {
   'global-payroll-properties': globalPayrollProperties,
-  'react16-instances': react16Instances,
+  'no-restricted-call-result-use': noRestrictedCallResultUse,
   'string-literal-blacklist': stringLiteralBlacklist,
   'team-annotations': teamAnnotations,
 };
