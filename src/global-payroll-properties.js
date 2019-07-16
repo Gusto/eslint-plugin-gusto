cd23f1cfc07a8dc364445ac09434f0846035e778
Mark Wheeler <mark.wheeler@gusto.com>
2019-07-15 17:46:50 -0700

    observe updated rules

--- src/global-payroll-properties.js
+++ src/global-payroll-properties.js
@@ -5,41 +5,40 @@ function isIdentifier(node, name) {
 function getPropertyName(node) {
   const { property } = node;
   switch (property.type) {
-  case 'Identifier':
-    return property.name;
-  case 'Literal':
-    return property.value;
-  default:
-    return null;
+    case 'Identifier':
+      return property.name;
+    case 'Literal':
+      return property.value;
+    default:
+      return null;
   }
 }
 
 function getRootMemberExpression(node) {
-  while (node.parent.type === 'MemberExpression') {
-    node = node.parent;
+  let member = node;
+  while (member.parent.type === 'MemberExpression') {
+    member = member.parent;
   }
 
-  return node;
+  return member;
 }
 
 function globalPayrollProperties(context) {
   const exceptions = (context.options[0] || {}).exceptions || [];
 
   return {
-    MemberExpression: function(node) {
+    MemberExpression: node => {
       if (isIdentifier(node.object, 'Payroll')) {
         const propertyName = getPropertyName(node);
         if (exceptions.indexOf(propertyName) < 0) {
-          const fullProperty = context
-            .getSourceCode()
-            .getText(getRootMemberExpression(node));
+          const fullProperty = context.getSourceCode().getText(getRootMemberExpression(node));
           context.report({
             node: node.property,
-            message: `Must import/require Payroll property '${fullProperty}'.`
+            message: `Must import/require Payroll property '${fullProperty}'.`,
           });
         }
       }
-    }
+    },
   };
 }
 
@@ -50,12 +49,12 @@ globalPayrollProperties.schema = [
       exceptions: {
         type: 'array',
         items: {
-          type: 'string'
-        }
-      }
+          type: 'string',
+        },
+      },
     },
-    additionalProperties: false
-  }
+    additionalProperties: false,
+  },
 ];
 
 export default globalPayrollProperties;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import globalPayrollProperties from './global-payroll-properties';
 import stringLiteralBlacklist from './string-literal-blacklist';
 import teamAnnotations from './team-annotations';
 
+// eslint-disable-next-line import/prefer-default-export
 export const rules = {
   'global-payroll-properties': globalPayrollProperties,
   'string-literal-blacklist': stringLiteralBlacklist,
-  'team-annotations': teamAnnotations
+  'team-annotations': teamAnnotations,
 };
