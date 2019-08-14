7a03b6de35e4bdd232eeb2a94e81ce950b055fa6
Mark Wheeler <mark.wheeler@gusto.com>
2019-08-14 14:16:25 -0700

    comment suggestion # -> //

--- tests/lib/rules/team-annotations.js
+++ tests/lib/rules/team-annotations.js
@@ -3,77 +3,83 @@ import { RuleTester } from 'eslint';
 
 const options = [
   {
-    missions: [ 'Mission' ],
-    teams: [ 'Team' ],
-  }
+    missions: ['Mission'],
+    teams: ['Team'],
+  },
 ];
 
 const ruleTester = new RuleTester({
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
-    }
-  }
+      jsx: true,
+    },
+  },
 });
 ruleTester.run('team-annotations', rule, {
   valid: [
     {
-      code: [
-        '// @mission Mission',
-        '// @team Team'
-      ].join("\n"),
+      code: ['// @mission Mission', '// @team Team'].join('\n'),
       options,
       env: {
-        es6: true
+        es6: true,
       },
       ecmaFeatures: {
-        jsx: true
-      }
+        jsx: true,
+      },
     },
   ],
   invalid: [
     {
-      code: [
-        '// @mission Mission'
-      ].join("\n"),
+      code: ['// @mission Mission'].join('\n'),
       options,
       env: {
-        es6: true
+        es6: true,
       },
       ecmaFeatures: {
-        jsx: true
-      },
-      errors: [{ message: "A team annotation is required at the second line of the file (i.e. `# @team Owning Team`)." }]
-    },
-    {
-      code: [
-        '',
-        '// @team Team'
-      ].join("\n"),
-      options,
-      env: {
-        es6: true
-      },
-      ecmaFeatures: {
-        jsx: true
-      },
-      errors: [{ message: "A mission annotation is required at the top of the file (i.e. `# @mission Owning Mission`)." }]
-    },
-    {
-      code: [
-        'function print1() { console.log(1); }',
-      ].join("\n"),
-      options,
-      env: {
-        es6: true
-      },
-      ecmaFeatures: {
-        jsx: true
+        jsx: true,
       },
       errors: [
-        { message: "A mission annotation is required at the top of the file (i.e. `# @mission Owning Mission`)." },
-        { message: "A team annotation is required at the second line of the file (i.e. `# @team Owning Team`)." }
-      ]
+        {
+          message:
+            'A team annotation is required at the second line of the file (i.e. `// @team Owning Team`).',
+        },
+      ],
     },
-  ]
+    {
+      code: ['', '// @team Team'].join('\n'),
+      options,
+      env: {
+        es6: true,
+      },
+      ecmaFeatures: {
+        jsx: true,
+      },
+      errors: [
+        {
+          message:
+            'A mission annotation is required at the top of the file (i.e. `// @mission Owning Mission`).',
+        },
+      ],
+    },
+    {
+      code: ['function print1() { console.log(1); }'].join('\n'),
+      options,
+      env: {
+        es6: true,
+      },
+      ecmaFeatures: {
+        jsx: true,
+      },
+      errors: [
+        {
+          message:
+            'A mission annotation is required at the top of the file (i.e. `// @mission Owning Mission`).',
+        },
+        {
+          message:
+            'A team annotation is required at the second line of the file (i.e. `// @team Owning Team`).',
+        },
+      ],
+    },
+  ],
 });
