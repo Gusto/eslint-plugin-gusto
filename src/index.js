31f4099b6734231277ce369b3435fd99f18bf969
Jordan Cutler <jordan.cutler@gusto.com>
2021-12-02 08:50:00 -0800

    [Gusto-Eslint] Add rule to allow ZP to use TS as default (#30)
    * [Gusto-Eslint] Add rule to allow ZP to use TS as default
    
    * update lint

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import globalPayrollProperties from './global-payroll-properties';
 import noRestrictedCallResultUse from './no-restricted-call-result-use';
 import stringLiteralBlacklist from './string-literal-blacklist';
 import teamAnnotations from './team-annotations';
+import preferTypescriptOverJavascript from './prefer-typescript-over-javascript';
 
 // eslint-disable-next-line import/prefer-default-export
 export const rules = {
@@ -9,4 +10,5 @@ export const rules = {
   'no-restricted-call-result-use': noRestrictedCallResultUse,
   'string-literal-blacklist': stringLiteralBlacklist,
   'team-annotations': teamAnnotations,
+  'prefer-typescript-over-javascript': preferTypescriptOverJavascript,
 };
